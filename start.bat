085a70b46507d2f09d8161d228f7842f8c9d5493
Darsh Bhatnagar <darshbhatnagar23@gmail.com>
2026-01-15 14:38:17 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,16 +1,14 @@
 @echo off
 
 REM Start backend
-cd /d %~dp0\backend
+cd /d %~dp0\backend || exit /b
 python -m pip install -r requirements.txt
 start cmd /k "python app.py"
 
-REM Wait a few seconds for backend to start
+REM Give backend time to start
 timeout /t 5 /nobreak
 
 REM Start frontend
-cd /d %~dp0\frontend
+cd /d %~dp0\frontend || exit /b
 npm install
 start cmd /k "npm start"
-
-pause
