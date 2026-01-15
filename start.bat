2a1ea7d76398ae00d04b183604a15289cd8515e0
Darsh Bhatnagar <darshbhatnagar23@gmail.com>
2026-01-15 14:53:39 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,15 +1,10 @@
 @echo off
 
-REM Start backend
 cd /d %~dp0\backend || exit /b
 python -m pip install -r requirements.txt
 start cmd /k "python app.py"
 
-REM Wait for backend
-timeout /t 5 /nobreak
 
-REM Start frontend (non-interactive)
 cd /d %~dp0\frontend || exit /b
-set BROWSER=none
 set CI=true
 start cmd /k "npm start"
